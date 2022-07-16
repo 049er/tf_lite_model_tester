b4037e5b10a69315050945e7936d42ba48c019d8
Werner <werner@scholtzonline.net>
2022-07-16 08:56:37 +0200

    initial commit

---
+++ lib/model_testing/camera_view.dart
@@ -0,0 +1,200 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+
+import '../../main.dart';
+
+enum ScreenMode { liveFeed, gallery }
+
+class CameraView extends StatefulWidget {
+  const CameraView(
+      {Key? key,
+      required this.title,
+      required this.customPaint,
+      this.text,
+      required this.onImage,
+      this.onScreenModeChanged,
+      this.initialDirection = CameraLensDirection.back})
+      : super(key: key);
+
+  final String title;
+  final CustomPaint? customPaint;
+  final String? text;
+  final Function(InputImage inputImage) onImage;
+  final Function(ScreenMode mode)? onScreenModeChanged;
+  final CameraLensDirection initialDirection;
+
+  @override
+  _CameraViewState createState() => _CameraViewState();
+}
+
+class _CameraViewState extends State<CameraView> {
+  CameraController? _controller;
+  int _cameraIndex = 0;
+  double zoomLevel = 0.0, minZoomLevel = 0.0, maxZoomLevel = 0.0;
+  bool flash = false;
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < cameras.length; i++) {
+      if (cameras[i].lensDirection == widget.initialDirection) {
+        _cameraIndex = i;
+      }
+    }
+    _startLiveFeed();
+  }
+
+  @override
+  void dispose() {
+    _stopLiveFeed();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.info_outline_rounded),
+          ),
+        ],
+      ),
+      body: _body(),
+      floatingActionButton: _floatingActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+    );
+  }
+
+  Widget? _floatingActionButton() {
+    if (cameras.length == 1) return null;
+    return FloatingActionButton(
+      backgroundColor: Colors.deepOrange,
+      heroTag: 'flash',
+      child: Icon(
+        Platform.isIOS
+            ? Icons.flip_camera_ios_outlined
+            : Icons.flash_on_rounded,
+      ),
+      onPressed: () {
+        if (flash == true) {
+          _controller!.setFlashMode(FlashMode.off);
+          flash = false;
+        } else {
+          flash = true;
+          _controller!.setFlashMode(FlashMode.torch);
+        }
+      },
+    );
+  }
+
+  Widget _body() {
+    Widget body;
+
+    body = _liveFeedBody();
+
+    return body;
+  }
+
+  Widget _liveFeedBody() {
+    if (_controller?.value.isInitialized == false) {
+      return Container();
+    }
+    return Container(
+      color: Colors.black,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          CameraPreview(_controller!),
+          if (widget.customPaint != null) widget.customPaint!,
+        ],
+      ),
+    );
+  }
+
+  Future _startLiveFeed() async {
+    final camera = cameras[_cameraIndex];
+    _controller = CameraController(
+      camera,
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+
+    await _controller?.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      _controller?.getMinZoomLevel().then((value) {
+        zoomLevel = value;
+        minZoomLevel = value;
+      });
+      _controller?.getMaxZoomLevel().then((value) {
+        maxZoomLevel = value;
+      });
+      _controller?.startImageStream(_processCameraImage);
+      setState(() {});
+    });
+  }
+
+  Future _stopLiveFeed() async {
+    await _controller?.stopImageStream();
+    await _controller?.dispose();
+    _controller = null;
+  }
+
+  Future _processCameraImage(CameraImage image) async {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (Plane plane in image.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize =
+        Size(image.width.toDouble(), image.height.toDouble());
+
+    final camera = cameras[_cameraIndex];
+    final imageRotation =
+        InputImageRotationValue.fromRawValue(camera.sensorOrientation) ??
+            InputImageRotation.rotation0deg;
+
+    final inputImageFormat =
+        InputImageFormatValue.fromRawValue(image.format.raw) ??
+            InputImageFormat.nv21;
+
+    final planeData = image.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation,
+      inputImageFormat: inputImageFormat,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    widget.onImage(inputImage);
+  }
+}
